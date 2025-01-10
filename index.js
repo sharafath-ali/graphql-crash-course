d0fb192d20c344cb178ae80c2d2bbce761fd4a40
sharafath ali <sharafathaliofficial@gmail.com>
2025-01-10 23:29:47 +0530

    basic get in graphql

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema.js';
+import _db from './_db.js';
 
-const server = new ApolloServer({
-  typeDefs: `
-    type Query {
-      hello: String
-    }
-  `,
-  resolvers: {
-    Query: {
-      hello: () => 'Hello world!',
+const resolvers = {
+  Query: {
+    games: () => _db.games,
+    reviews: () => _db.reviews,
+    authors: () => _db.authors,
+    game: (parent, args) => {
+      return _db.games.find((game) => game.id === args.id);
+    },
+    review: (parent, args) => {
+      return _db.reviews.find((review) => review.id === args.id);
+    },
+    author: (parent, args) => {
+      return _db.authors.find((author) => author.id === args.id);
     },
   },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
+  cors: {
+    origin: ['https://studio.apollographql.com'], // Allow Apollo Sandbox
+    credentials: true, // Optional if using authentication
+  },
 });
-console.log(`🚀  Server ready at: ${url}`);
+
+console.log(`🚀 Server ready at: ${url}`);
