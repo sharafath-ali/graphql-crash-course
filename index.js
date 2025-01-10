e20609908e3fb5bcd279dd0e7c4652e2f760217b
sharafath ali <sharafathaliofficial@gmail.com>
2025-01-10 20:38:27 +0530

    basic apollo server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const server = new ApolloServer({
+  typeDefs: `
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello world!',
+    },
+  },
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
