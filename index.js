e0664d2ad83f9c6dd2a9f4c6124dfd719c2c3caa
sharafath ali <sharafathaliofficial@gmail.com>
2025-01-12 00:12:27 +0530

    graph mutation

--- index.js
+++ index.js
@@ -36,6 +36,84 @@ const resolvers = {
       return _db.authors.find((author) => author.id === parent.author_id);
     },
   },
+  Mutation: {
+    createGame: (parent, args) => {
+      const newGame = {
+        id: _db.games.length + 1,
+        title: args.title,
+        platform: args.platform,
+      };
+      _db.games.push(newGame);
+      return newGame;
+    },
+    deleteGame: (parent, args) => {
+      const deletedGame = _db.games.find((game) => game.id === args.id);
+      _db.games = _db.games.filter((game) => game.id !== args.id);
+      return deletedGame;
+    },
+    updateGame: (parent, args) => {
+      const updatedGame = _db.games.find((game) => game.id === args.id);
+      updatedGame.title = args.title;
+      updatedGame.platform = args.platform;
+      return updatedGame;
+    },
+    createReview: (parent, args) => {
+      const newReview = {
+        id: _db.reviews.length + 1,
+        rating: args.rating,
+        content: args.content,
+        author_id: args.author_id,
+        game_id: args.game_id,
+      };
+      _db.reviews.push(newReview);
+      return newReview;
+    },
+    deleteReview: (parent, args) => {
+      const deletedReview = _db.reviews.find((review) => review.id === args.id);
+      _db.reviews = _db.reviews.filter((review) => review.id !== args.id);
+      return deletedReview;
+    },
+    updateReview: (parent, args) => {
+      const updatedReview = _db.reviews.find((review) => review.id === args.id);
+      updatedReview.rating = args.rating;
+      updatedReview.content = args.content;
+      updatedReview.author_id = args.author_id;
+      updatedReview.game_id = args.game_id;
+      return updatedReview;
+    },
+    createAuthor: (parent, args) => {
+      const newAuthor = {
+        id: _db.authors.length + 1,
+        name: args.name,
+        verified: args.verified,
+      };
+      _db.authors.push(newAuthor);
+      return newAuthor;
+    },
+    deleteAuthor: (parent, args) => {
+      const deletedAuthor = _db.authors.find((author) => author.id === args.id);
+      _db.authors = _db.authors.filter((author) => author.id !== args.id);
+      return deletedAuthor;
+    },
+    updateAuthor: (parent, args) => {
+      const updatedAuthor = _db.authors.map((author) => {
+        if (author.id === args.id) {
+          console.log({ ...author, ...args });
+          return {
+            ...author,
+            name: args.name,
+            verified: args.verified,
+          }
+        } else {
+          return author
+        }
+      });
+
+      _db.authors = updatedAuthor;
+
+      return _db.authors.find((author) => author.id === args.id);
+    },
+  },
 };
 
 const server = new ApolloServer({
--- schema.js
+++ schema.js
@@ -26,6 +26,17 @@ export const typeDefs = `#graphql
     authors: [Author]
     author(id: ID!) : Author
   }
+  type Mutation {
+    createGame(title: String!, platform: [String!]!): Game
+    createReview(rating: Int!, content: String!, author_id: ID!, game_id: ID!): Review
+    createAuthor(name: String!, verified: Boolean!): Author
+    updateGame(id: ID!, title: String!, platform: [String!]!): Game
+    updateReview(id: ID!, rating: Int!, content: String!, author_id: ID!, game_id: ID!): Review
+    updateAuthor(id: ID!, name: String!, verified: Boolean!): Author
+    deleteGame(id: ID!): Game
+    deleteReview(id: ID!): Review
+    deleteAuthor(id: ID!): Author
+  }
 `
 
 //int float string boolean ID these are the basic types in graphql
